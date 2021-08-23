55f09e430bc28025f7cd27372e819b9a0214875a
root-marco <rootslowed@gmail.com>
2021-08-23 11:14:55 -0300

    spring app tests

---
+++ src/test/java/com/slowed/employeemanager/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slowed.employeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
