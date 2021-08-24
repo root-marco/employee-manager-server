bda12087ba7730dddc673c7a879a71c90cdca1a6
root-marco <rootslowed@gmail.com>
2021-08-24 14:43:26 -0300

    cors changed allowed origins

--- src/main/java/com/slowed/employeemanager/Application.java
+++ src/main/java/com/slowed/employeemanager/Application.java
@@ -21,7 +21,7 @@ public class Application {
   public CorsFilter corsFilter() {
     CorsConfiguration corsConfiguration = new CorsConfiguration();
     corsConfiguration.setAllowCredentials(true);
-    corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
+    corsConfiguration.setAllowedOrigins(List.of("employee-manager-45.herokuapp.com", "https://employee-manager-45.herokuapp.com/"));
     corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
         "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
         "Access-Control-Request-Method", "Access-Control-Request-Headers"));
