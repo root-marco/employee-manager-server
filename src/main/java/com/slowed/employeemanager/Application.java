63c38ea7fcf7a3a8aaad64330260d34cb27587b5
root-marco <66764981+root-marco@users.noreply.github.com>
2021-08-31 21:26:06 -0300

    Update Application.java

--- src/main/java/com/slowed/employeemanager/Application.java
+++ src/main/java/com/slowed/employeemanager/Application.java
@@ -21,7 +21,7 @@ public class Application {
   public CorsFilter corsFilter() {
     CorsConfiguration corsConfiguration = new CorsConfiguration();
     corsConfiguration.setAllowCredentials(true);
-    corsConfiguration.setAllowedOrigins(List.of("https://employee-manager-client.vercel.app/"));
+    corsConfiguration.setAllowedOrigins(List.of("https://employee-manager-client.vercel.app/", "https://employee-manager-client.vercel.app"));
     corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
         "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
         "Access-Control-Request-Method", "Access-Control-Request-Headers"));
